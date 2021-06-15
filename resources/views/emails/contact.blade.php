2bbe04669aa0322c4de9ec014bd168808776dfc5
FLTOki <jim.musyoki@strathmore.edu>
2021-06-15 16:28:29 +0300

    initial changes

---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Email') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <form action="" method="" class="w-full-wrap">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
